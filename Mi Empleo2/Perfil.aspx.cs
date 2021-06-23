c625472f1c8c1ca9755b29c4e6657f8be30fe1e7
Josh <hdzjorgeoficial@gmail.com>
2021-06-22 22:09:50 -0600

    J1

--- Mi Empleo2/Perfil.aspx.cs
+++ Mi Empleo2/Perfil.aspx.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
@@ -60,17 +61,17 @@ namespace Mi_Empleo2
             System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
             return image;
         }
-             
+
         public async Task<UserModel> getUser()
         {
             var token = Session["token"];
             UserModel serviceResult = new UserModel();
-            string uri = ConfigurationManager.AppSettings["production"] + "users/detail/";                                    
-            HttpClient httpClient = new HttpClient();            
+            string uri = ConfigurationManager.AppSettings["production"] + "users/detail/";
+            HttpClient httpClient = new HttpClient();
             try
             {
                 using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri))
-                {                   
+                {
                     request.Headers.Add("Authorization", "Token " + token);
                     using (HttpResponseMessage response = await httpClient.SendAsync(request))
                     {
@@ -81,10 +82,10 @@ namespace Mi_Empleo2
                                 string responseStringContent = await content.ReadAsStringAsync();
                                 serviceResult = Newtonsoft.Json.JsonConvert.DeserializeObject<UserModel>(responseStringContent);
                                 return serviceResult;
-                             }                            
+                            }
                         }
-                    }                    
-                }                
+                    }
+                }
             }
             catch (WebException ex)
             {
@@ -133,79 +134,107 @@ namespace Mi_Empleo2
         protected void upload_Click(object sender, EventArgs e)
         {
             UserModel userModel = new UserModel();
-            userModel.first_name = TBName.Text;
-            userModel.last_name=  TBApellido.Text;
-            userModel.email = TBEmail.Text;
-            userModel.profession = TBProfe.Text;
-            userModel.username = LBUser.Text;
-            userModel.phone = TBPhone.Text;
-            if (FileUpload1.FileName != "")
+            if (TBName.Text != "")
             {
-                System.IO.Stream fs = FileUpload1.PostedFile.InputStream;
-                System.IO.BinaryReader br = new System.IO.BinaryReader(fs);
-                Byte[] bytes = br.ReadBytes((Int32)fs.Length);
-                string base64String = Convert.ToBase64String(bytes, 0, bytes.Length);
-                string imagen = "data:image/png;base64," + base64String;
-                userModel.image = imagen;
+                userModel.first_name = TBName.Text;
+                if (true)
+                {
+                    userModel.last_name = TBApellido.Text;
+                    if (true)
+                    {
+                        userModel.email = TBEmail.Text;
+                        if (true)
+                        {
+                            userModel.profession = TBProfe.Text;
+                            if (true)
+                            {
+                                userModel.username = LBUser.Text;
+                                if (!Regex.Match(TBName.Text, "").Success)
+                                {
+                                    userModel.phone = TBPhone.Text;
+
+                                    if (true)
+                                    {
+                                        userModel.experience = TBEx.Text;
+                                        if (true)
+                                        {
+                                            userModel.place = DDLugar.Text;
+                                            if (FileUpload1.FileName != "")
+                                            {
+                                                System.IO.Stream fs = FileUpload1.PostedFile.InputStream;
+                                                System.IO.BinaryReader br = new System.IO.BinaryReader(fs);
+                                                Byte[] bytes = br.ReadBytes((Int32)fs.Length);
+                                                string base64String = Convert.ToBase64String(bytes, 0, bytes.Length);
+                                                string imagen = "data:image/png;base64," + base64String;
+                                                userModel.image = imagen;
+                                            }
+                                            else
+                                            {
+                                                userModel.image = image.ImageUrl;
+                                            }
+                                            SkillsModel skills = new SkillsModel()
+                                            {
+                                                skill1 = TBSkill1.Text,
+                                                skill2 = TBSkill2.Text,
+                                                skill3 = TBSkill3.Text,
+                                                skill4 = TBSkill4.Text
+                                            };
+                                            userModel.skills = JsonConvert.SerializeObject(skills);
+
+                                            SitesModel sites = new SitesModel()
+                                            {
+                                                site1 = TBSite1.Text,
+                                                site2 = TBSite2.Text,
+                                                site3 = TBSite3.Text,
+                                                site4 = TBSite4.Text
+                                            };
+                                            userModel.sites = JsonConvert.SerializeObject(sites);
+
+                                            string[] validFileTypes = { "pdf", "doc" };
+                                            string ext = System.IO.Path.GetExtension(FileUpload2.PostedFile.FileName);
+                                            bool isValidFile = false;
+                                            for (int i = 0; i < validFileTypes.Length; i++)
+                                            {
+                                                if (ext == "." + validFileTypes[i])
+                                                {
+                                                    isValidFile = true;
+                                                    break;
+                                                }
+                                            }
+                                            if (!isValidFile)
+                                            {
+                                                ClientScript.RegisterStartupScript(GetType(), "Message", @"<SCRIPT LANGUAGE='javascript'>swal(""Fallo!"", ""Es necesario que el archivo sea PDF!"", ""error"");</script>");
+                                            }
+                                            else
+                                            {
+                                                System.IO.Stream fs = FileUpload2.PostedFile.InputStream;
+                                                System.IO.BinaryReader br = new System.IO.BinaryReader(fs);
+                                                Byte[] bytes = br.ReadBytes((Int32)fs.Length);
+                                                string base64String = Convert.ToBase64String(bytes, 0, bytes.Length);
+                                                userModel.cv = "data:application/pdf;base64," + base64String;
+                                                var task = Task.Run(async () => await updateUser(userModel));
+                                                bool result = task.Result;
+                                                if (result)
+                                                {
+                                                    ClientScript.RegisterStartupScript(GetType(), "Message", @"<SCRIPT LANGUAGE='javascript'>swal(""Exitoso!"", ""Se guardo la información correctamente!"", ""success"");</script>");
+                                                }
+                                                else
+                                                {
+                                                    ClientScript.RegisterStartupScript(GetType(), "Message", @"<SCRIPT LANGUAGE='javascript'>swal(""Fallo!"", ""Error al guardar la información!"", ""error"");</script>");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }                        
+                    }
+                }
             }
             else
             {
-                userModel.image = image.ImageUrl;
+                ClientScript.RegisterStartupScript(GetType(), "Message", @"<SCRIPT LANGUAGE='javascript'>swal(""Fallo!"", ""Error al guardar la información porque el nombre esta incorrecto!"", ""error"");</script>");
             }
-            SkillsModel skills = new SkillsModel()
-            {
-                skill1 = TBSkill1.Text,
-                skill2 = TBSkill2.Text,
-                skill3 = TBSkill3.Text,
-                skill4 = TBSkill4.Text
-            };
-            userModel.skills = JsonConvert.SerializeObject(skills);
-
-            SitesModel sites = new SitesModel()
-            {
-                site1 = TBSite1.Text,
-                site2 = TBSite2.Text,
-                site3 = TBSite3.Text,
-                site4 = TBSite4.Text
-            };
-            userModel.sites = JsonConvert.SerializeObject(sites);
-            userModel.experience = TBEx.Text;
-            userModel.place = DDLugar.Text;
-
-            string[] validFileTypes = { "pdf", "doc"};
-            string ext = System.IO.Path.GetExtension(FileUpload2.PostedFile.FileName);
-            bool isValidFile = false;
-            for (int i = 0; i < validFileTypes.Length; i++)
-            {
-                if (ext == "." + validFileTypes[i])
-                {
-                    isValidFile = true;
-                    break;
-                }
-            }
-            if (!isValidFile)
-            {
-                ClientScript.RegisterStartupScript(GetType(), "Message", @"<SCRIPT LANGUAGE='javascript'>swal(""Fallo!"", ""Es necesario que el archivo sea PDF!"", ""error"");</script>");
-            }
-            else
-            {
-                System.IO.Stream fs = FileUpload2.PostedFile.InputStream;
-                System.IO.BinaryReader br = new System.IO.BinaryReader(fs);
-                Byte[] bytes = br.ReadBytes((Int32)fs.Length);
-                string base64String = Convert.ToBase64String(bytes, 0, bytes.Length);
-                userModel.cv = "data:application/pdf;base64," + base64String;                
-                var task = Task.Run(async () => await updateUser(userModel));
-                bool result = task.Result;
-                if (result)
-                {
-                    ClientScript.RegisterStartupScript(GetType(), "Message", @"<SCRIPT LANGUAGE='javascript'>swal(""Exitoso!"", ""Se guardo la información correctamente!"", ""success"");</script>");
-                }
-                else
-                {
-                    ClientScript.RegisterStartupScript(GetType(), "Message", @"<SCRIPT LANGUAGE='javascript'>swal(""Fallo!"", ""Error al guardar la información!"", ""error"");</script>");
-                }
-                
-            }            
         }
 
         protected void recargar_Click(object sender, EventArgs e)
